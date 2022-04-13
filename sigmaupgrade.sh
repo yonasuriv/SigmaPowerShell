d8cbf09dc5ad121f6c76bdbce6017f1f8b82c285
Jonathan Di Rico <diricojonathan@gmail.com>
2022-04-13 15:48:45 +0000

    Updated script

--- sigmaupgrade.sh
+++ sigmaupgrade.sh
@@ -1,3 +1,17 @@
+logo (){
+echo Starting...
+sleep 2
+clear
+echo
+echo "__________                              _________.__                          _________.__           .__  .__   ";
+echo "\______   \______  _  __ ___________   /   _____/|__| ____   _____ _____     /   _____/|  |__   ____ |  | |  |  ";
+echo " |     ___/  _ \ \/ \/ // __ \_  __ \  \_____  \ |  |/ ___\ /     \\__  \    \_____  \ |  |  \_/ __ \|  | |  |  ";
+echo " |    |  (  <_> )     /\  ___/|  | \/  /        \|  / /_/  >  Y Y  \/ __ \_  /        \|   Y  \  ___/|  |_|  |__";
+echo " |____|   \____/ \/\_/  \___  >__|    /_______  /|__\___  /|__|_|  (____  / /_______  /|___|  /\___  >____/____/";
+echo "                            \/                \/   /_____/       \/     \/          \/      \/     \/           ";
+echo 
+}
+
 zsh_install (){
 sudo apt -y install zsh
 clear
@@ -17,22 +31,7 @@ clear
 powerlevel (){
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 sleep 2
-cp -rT ./core ~/
 clear
-}
-
-logo (){
-echo Starting...
-sleep 2
-clear
-echo
-echo "__________                              _________.__                          _________.__           .__  .__   ";
-echo "\______   \______  _  __ ___________   /   _____/|__| ____   _____ _____     /   _____/|  |__   ____ |  | |  |  ";
-echo " |     ___/  _ \ \/ \/ // __ \_  __ \  \_____  \ |  |/ ___\ /     \\__  \    \_____  \ |  |  \_/ __ \|  | |  |  ";
-echo " |    |  (  <_> )     /\  ___/|  | \/  /        \|  / /_/  >  Y Y  \/ __ \_  /        \|   Y  \  ___/|  |_|  |__";
-echo " |____|   \____/ \/\_/  \___  >__|    /_______  /|__\___  /|__|_|  (____  / /_______  /|___|  /\___  >____/____/";
-echo "                            \/                \/   /_____/       \/     \/          \/      \/     \/           ";
-echo 
 }
 
 zsh_install
@@ -42,6 +41,7 @@ logo
 echo 
 echo "If you want to do an advanced customization of your shell, please run: p10k configure"
 echo
+cp -rT ./core ~/
 echo "exec zsh" > ~/.bashrc
 exec zsh
 
