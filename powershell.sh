326897c8c5c9a6cd7e299eebc5edbf7cedcae8ce
Jonathan Di Rico <diricojonathan@gmail.com>
2022-04-13 00:01:31 +0000

    Updated core

--- powershell.sh
+++ powershell.sh
@@ -5,19 +5,19 @@ clear
 
 fonts (){
 mkdir -p ~/.local/share/fonts 
-cp "MesloLGS NF Regular.ttf" ~/.local/share/fonts
-cp "MesloLGS NF Bold.ttf" ~/.local/share/fonts
-cp "MesloLGS NF Italic.ttf" ~/.local/share/fonts
-cp "MesloLGS NF Bold Italic.ttf" ~/.local/share/fonts
-fc-cache -f -v
+cp /fonts/* ~/.local/share/fonts
+# cp "MesloLGS NF Regular.ttf" ~/.local/share/fonts
+# cp "MesloLGS NF Bold.ttf" ~/.local/share/fonts
+# cp "MesloLGS NF Italic.ttf" ~/.local/share/fonts
+# cp "MesloLGS NF Bold Italic.ttf" ~/.local/share/fonts
+# c-cache -f -v
 clear
 }
 
 powerlevel (){
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-cp .p10k.zsh ~/
-cp .zshrc ~/
+cp /core/* ~/
 clear
 }
 
